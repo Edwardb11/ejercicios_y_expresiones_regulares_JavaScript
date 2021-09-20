753ed9315d775e8cc9b1b7d1d9f53c9196964cbd
Edward Brito <edwardbrito11@hotmail.com>
2021-09-19 21:07:51 -0400

    Casos_comunes     Validando_correo_electronico     Validando_usuario_de_Twitter     Otros_casos
    Changes to be committed:
    	new file:   Casos_comunes/Otros_casos/app.js
    	new file:   Casos_comunes/Otros_casos/base.css
    	new file:   Casos_comunes/Otros_casos/index.html
    	new file:   Casos_comunes/Validando_correo_electronico/app.js
    	new file:   Casos_comunes/Validando_correo_electronico/base.css
    	new file:   Casos_comunes/Validando_correo_electronico/index.html
    	new file:   Casos_comunes/Validando_usuario_de_Twitter/app.js
    	new file:   Casos_comunes/Validando_usuario_de_Twitter/base.css
    	new file:   Casos_comunes/Validando_usuario_de_Twitter/index.html
    
    Author
        Edward Brito Diaz

---
+++ Casos_comunes/Validando_correo_electronico/app.js
@@ -0,0 +1,19 @@
+$(function () {
+    var contactForm = document.getElementById('contactForm');
+
+    contactForm.onsubmit = function (e) {
+        e.preventDefault();
+
+        console.log('¿El correo es válido?', isValidEmail());
+    }
+
+    var isValidEmail = function () {
+        var email = document.getElementById('email');
+        return /^\w+([\.\+\-]?\w+)*@([\.-]?\w+)*(\.\w{2,4})+$/.test(email.value)
+        //\w+ cualquier tipo de letra o numero y k se repita
+        //([\.\+\-]? agrupado de valores que puede existir y el signo ? significa que puede o no estar presente y w+ dice que puede exitri
+        //cualquier carater 1 o mas veces
+
+    }
+
+});
